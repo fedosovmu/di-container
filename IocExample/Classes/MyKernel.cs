05ad31868d97c63c2f052df32b5f9e421f48b201
fedosovmu <fedosovmu@gmail.com>
2018-05-26 03:28:13 +0500

    final

---
+++ IocExample/Classes/MyKernel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IocExample.Classes
+{
+    public static class MyKernel
+    {
+        private static Utils utils = new Utils();
+        private static readonly Dictionary<Type, Type> types = new Dictionary<Type, Type>();
+        private static readonly Dictionary<Type, object> typeInstances = new Dictionary<Type, object>();
+
+
+        public static void Bind<TContract, TImplementation>()
+        {
+            types[typeof(TContract)] = typeof(TImplementation);
+        }
+
+
+        public static void Bind<TContract, TImplementation>(TImplementation instance)
+        {
+            typeInstances[typeof(TContract)] = instance;
+        }
+
+
+        public static T Resolve<T>()
+        {
+            return (T)Resolve(typeof(T));
+        }
+
+
+        public static object Resolve(Type contract)
+        {
+            if (typeInstances.ContainsKey(contract))
+            {
+                return typeInstances[contract];
+            }
+            else
+            {
+                Type implementation = types[contract];
+
+                ConstructorInfo constructor = Utils.GetSingleConstructor(implementation);
+                ParameterInfo[] constructorParameters = constructor.GetParameters();
+
+                if (constructorParameters.Length == 0)
+                {
+                    return Activator.CreateInstance(implementation);
+                }
+
+                List<object> parameters = new List<object>(constructorParameters.Length);
+
+                foreach (ParameterInfo parameterInfo in constructorParameters)
+                {
+                    parameters.Add(Resolve(parameterInfo.ParameterType));
+                }
+
+                return constructor.Invoke(parameters.ToArray());
+            }
+        }
+
+        public static object Inject<T>()
+        {
+            return typeInstances[typeof(T)];
+        }
+
+        public static object Get<T>()
+        {
+            return (T)Utils.CreateInstance(typeof(T));
+        }
+
+        
+        public static object Get<T>(List<object> parameters)
+        {
+            return (T)Utils.CreateInstance(typeof(T), parameters);
+        }
+
+
+    }
+}
---
+++ IocExample/Program.cs
@@ -0,0 +1,69 @@
+﻿using IocExample.Classes;
+using Ninject;
+using System;
+using IocExample;
+using System.Collections.Generic;
+
+
+namespace IocExample
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    var logger = new ConsoleLogger();
+        //    var sqlConnectionFactory = new SqlConnectionFactory("SQL Connection", logger);
+
+        //    var createUserHandler = new CreateUserHandler(new UserService(new QueryExecutor(sqlConnectionFactory), new CommandExecutor(sqlConnectionFactory), new CacheService(logger, new RestClient("API KEY"))), logger);
+
+        //    createUserHandler.Handle();
+        //    Console.ReadKey();
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    IKernel kernel = new StandardKernel();
+
+        //    kernel.Bind<ILogger>().To<ConsoleLogger>().InSingletonScope();
+        //    kernel.Bind<IConnectionFactory>()
+        //        .ToConstructor(k => new SqlConnectionFactory("SQL Connection", k.Inject<ILogger>()))
+        //        .InSingletonScope();
+
+
+        //    kernel.Bind<UserService>().To<UserService>();
+        //    kernel.Bind<QueryExecutor>().To<QueryExecutor>();
+        //    kernel.Bind<CommandExecutor>().To<CommandExecutor>();
+        //    kernel.Bind<CacheService>().To<CacheService>();
+        //    kernel.Bind<RestClient>().ToConstructor(k => new RestClient("API KEY"));
+
+        //    kernel.Bind<CreateUserHandler>().To<CreateUserHandler>();
+        //    var createUserHandler = kernel.Get<CreateUserHandler>();
+
+        //    createUserHandler.Handle();
+
+        //    Console.ReadKey();
+
+        //}
+
+
+        static void Main(string[] args)
+        {
+            MyKernel.Bind<ILogger, ConsoleLogger>(new ConsoleLogger());
+            MyKernel.Bind<IConnectionFactory, SqlConnectionFactory>((SqlConnectionFactory)MyKernel.Get<SqlConnectionFactory>(new List<object>() { "SQL Connection", MyKernel.Inject<ILogger>() }));
+
+            MyKernel.Bind<UserService, UserService>();
+            MyKernel.Bind<QueryExecutor, QueryExecutor>();
+            MyKernel.Bind<CommandExecutor, CommandExecutor>();
+            MyKernel.Bind<CacheService, CacheService>();
+
+            MyKernel.Bind<RestClient, RestClient>((RestClient)MyKernel.Get<RestClient>(new List<object>() { "API KEY" }));
+
+            MyKernel.Bind<CreateUserHandler, CreateUserHandler>();
+            var createUserHandler = MyKernel.Resolve<CreateUserHandler>();
+
+            createUserHandler.Handle();
+            Console.ReadKey();
+        }
+
+    }
+}
